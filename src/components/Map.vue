937f96145aa13addfaa784de9d2d579f1e9dc645
weimengying <weimengying515@163.com>
2020-09-15 23:20:48 +0800

    商家分布模块完成

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="com-container">
+  <div class="com-container" @dblclick="revertMap">
     <div class="com-chart" ref="map_ref"></div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import { getProvinceMapInfo } from '@/utils/map_utils'
 export default {
   data() {
     return {
       chartInstance: null,
-      allData: null
+      allData: null,
+      mapData: {} // 所获取的省份的地图矢量数据
     }
   },
   mounted () {
@@ -54,6 +56,25 @@ export default {
         }
       }
       this.chartInstance.setOption(initOption)
+      this.chartInstance.on('click', async arg => {
+        // arg.name 得到所点击的省份，这个省份他是中文
+        // console.log(arg)
+        const provinceInfo = getProvinceMapInfo(arg.name)
+        console.log(provinceInfo)
+        // 需要获取这个省份的地图矢量数据
+        // 判断当前所点击的这个省份的地图矢量数据在mapData中是否存在
+        if (!this.mapData[provinceInfo.key]) {
+          const ret = await axios.get('http://localhost:8999' + provinceInfo.path)
+          this.mapData[provinceInfo.key] = ret.data
+          this.$echarts.registerMap(provinceInfo.key, ret.data)
+        }
+        const changeOption = {
+          geo: {
+            map: provinceInfo.key
+          }
+        }
+        this.chartInstance.setOption(changeOption)
+      })
     },
     async getData () {
       // 获取服务器的数据，对this.allData进行赋值之后，调用updataChart方法更新图表
@@ -73,7 +94,8 @@ export default {
         // 如果想要在地图中显示散点的数据，所以我们需要给散点的图表增加一个配置，coordinateSystem:geo
         return {
           type: 'effectScatter',
-          rippleEffect: { // 涟漪效果
+          // 涟漪效果
+          rippleEffect: {
             scale: 5,
             brushType: 'stroke'
           },
@@ -91,9 +113,33 @@ export default {
       this.chartInstance.setOption(dataOption)
     },
     screenAdapter () {
-      const adapterOption = {}
+      const titleFontSize = this.$refs.map_ref.offsetWidth / 100 * 3.6
+      const adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: titleFontSize
+          }
+        },
+        legend: {
+          itemWidth: titleFontSize / 2,
+          itemHight: titleFontSize / 2,
+          itemGap: titleFontSize / 2,
+          textStyle: {
+            fontSize: titleFontSize / 2
+          }
+        }
+      }
       this.chartInstance.setOption(adapterOption)
       this.chartInstance.resize()
+    },
+    // 回到中国地图
+    revertMap () {
+      const revertOption = {
+        geo: {
+          map: 'china'
+        }
+      }
+      this.chartInstance.setOption(revertOption)
     }
   }
 }
